698101e4839e1ee7ffdb00f9a3774148cf945552
Jerome Raffin <raffin.jerome@gmail.com>
2018-02-19 19:43:00 +0100

    source code of lamagiedude.fr

---
+++ js/main.js
@@ -0,0 +1,1 @@
+function toggleMenu(){var a=document.getElementById("Site"),b=document.getElementById("ButtonMenu"),c=document.getElementById("Nav");c.onclick=function(){a.className=""},b.onclick=function(){a.className="Menu--isOpen"}}toggleMenu();
